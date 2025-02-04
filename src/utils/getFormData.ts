3faa46ea9f3725f363489663a0bafc69ca474f43
TYury <yurytorgashin@simple-it.pro>
2025-02-04 14:58:52 +0300

    add stylelint

--- src/utils/getFormData.ts
+++ src/utils/getFormData.ts
@@ -2,7 +2,9 @@ import { Block } from "../core/Block.ts";
 
 export const getFormData = (form: Block, excludeFields: string[] = []) => {
   const inputs = form.getLists().inputs;
-  return inputs.reduce((acc, { props }) => {
+  return inputs.reduce((acc: Record<string, string>, input) => {
+    const props = input.getProps() as { name: string; value: string };
+
     if (!excludeFields.includes(props.name)) {
       acc[props.name] = props.value;
     }
